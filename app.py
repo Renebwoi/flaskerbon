69da76c4516d8254a6db7097a4921003a9ad46bf
Reneboi <demiurgerene@gmail.com>
2024-06-14 05:16:50 +0100

    first

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, request, jsonify
+import json
+import sqlite3
+
+app = Flask(__name__)
+
+# books_list = [
+#    { "id":0,
+#     "author":"Chinua Achebe",
+#     "language":"English",
+#     "title":"Things Fall Apart",
+#     },
+#    {
+#        "id":1,
+#        "author":"Hans Christian Andersen",
+#        "language": "Danish",
+#        "title": "Fairy tales",
+#    },
+#    {
+#        "id":2,
+#        "author":"Jk Rowling",
+#        "language": "English",
+#        "title": "Harry Potter Sorcerer's stone",
+#    },
+#    {
+#        "id":3,
+#        "author":"Mary Anne",
+#        "language": "Danish",
+#        "title": "Little",
+#    },
+#    {
+#        "id":4,
+#        "author":"Jk Rowling",
+#        "language": "English",
+#        "title": "Harry Potter Prisoner of Azkaban",
+#    },
+#    {
+#        "id":5,
+#        "author":"Jk Rowling",
+#        "language": "English",
+#        "title": "Harry Potter Deathly Hallows 1",
+#    },
+#    {
+#        "id":6,
+#        "author":"Jk Rowling",
+#        "language": "English",
+#        "title": "Harry Potter Deathly Hallows 2",
+#    }
+# ]
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('books.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
+
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    conn = db_connection()
+    cursor = conn.cursor()
+    
+    if request.method == 'GET':
+        # if len(books_list) > 0:
+        #     return jsonify(books_list)
+        # else:
+        #     'Nothing Found', 404
+        cursor
+            
+    if request.method == 'POST':
+        new_author =request.form['author']
+        new_lang = request.form['language']
+        new_title = request.form['title']
+        iD = books_list[-1]['id']+1
+        
+        # creating an object with these values and appending to the books_list
+        new_obj = {
+            'id': iD,
+            'author': new_author,
+            'language': new_lang,
+            'title': new_title
+        }
+        books_list.append(new_obj)
+        return jsonify(books_list), 201
+    
+@app.route('/book/<int:id>', methods=['GET','PUT','DELETE'])
+def single_book(id):
+    if request.method == 'GET':
+        for book in books_list:
+            if book['id'] == id:
+                return jsonify(book)
+            pass
+    if request.method == 'PUT':
+        for book in books_list:
+            if book['id'] == id:
+                book['author'] = request.form['author']
+                book['language'] = request.form['language']
+                book['title'] = request.form['title']
+                updated_book = {
+                    'id': id,
+                    'author': book["author"],
+                    'language': book["language"],
+                    'title': book["title"]
+                }
+                return jsonify(updated_book)
+    if request.method == 'DELETE':
+        for index, book in enumerate(books_list):
+            if book['id'] == id:
+                books_list.pop(index)
+                return jsonify(books_list)
+
+def print_name(name):
+    return 'Hi , {}'.format(name)
+
+if __name__=='__main__':
+    app.run(debug=True)
