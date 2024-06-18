bbdb3fa91f6114807719c5a00ffd98b5abdc2179
Reneboi <demiurgerene@gmail.com>
2024-06-18 05:17:36 +0100

    implemented gemini

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import sqlite3
 from dotenv import load_dotenv
 import os
 
+import google.generativeai as genai
+
 app = Flask(__name__)
 
 books_list = [
@@ -185,7 +187,41 @@ def orders():
         else:
             'Nothing Found', 404  
         
-        
+# function to generate book summary. Later iterations will be to support file upload for better summary
+# and pay for OpenAI api so i can use that instead. For now the post request can simply ask for a 
+# summary of the book.
+@app.route('/summary', methods=['POST'])
+def summary():
+    if request.method == 'POST':
+            
+        load_dotenv()
+        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+        # Create the model
+        # See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+        generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 8192,
+        "response_mime_type": "text/plain",
+        }
+
+        model = genai.GenerativeModel(
+        model_name="gemini-1.5-pro",
+        generation_config=generation_config,
+        # safety_settings = Adjust safety settings
+        )
+
+        chat_session = model.start_chat(
+        history=[
+        ]
+        )
+
+        question = request.form['question']
+        response = chat_session.send_message(question)
+
+        return response.text       
 
 
 
